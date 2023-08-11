1e3291e2c90a0474041f219fb752a4d218f1c7cf
leo18bernese <leo18bermese@gmail.com>
2023-08-11 22:57:55 +0200

    updated task

--- bukkit/src/main/java/me/leoo/utils/bukkit/task/Tasks.java
+++ bukkit/src/main/java/me/leoo/utils/bukkit/task/Tasks.java
@@ -2,31 +2,32 @@ package me.leoo.utils.bukkit.task;
 
 import me.leoo.utils.bukkit.Utils;
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
 
 public class Tasks {
 
-    public static void run(Runnable task) {
-        Bukkit.getScheduler().runTask(Utils.get(), task);
+    public static BukkitTask run(Runnable task) {
+        return Bukkit.getScheduler().runTask(Utils.get(), task);
     }
 
-    public static void runAsync(Runnable task) {
-        Bukkit.getScheduler().runTaskAsynchronously(Utils.get(), task);
+    public static BukkitTask runAsync(Runnable task) {
+        return Bukkit.getScheduler().runTaskAsynchronously(Utils.get(), task);
     }
 
-    public static void runLater(Runnable task, long delay) {
-        Bukkit.getScheduler().runTaskLater(Utils.get(), task, delay);
+    public static BukkitTask runLater(Runnable task, long delay) {
+        return Bukkit.getScheduler().runTaskLater(Utils.get(), task, delay);
     }
 
-    public static void runAsyncLater(Runnable task, long delay) {
-        Bukkit.getScheduler().runTaskLaterAsynchronously(Utils.get(), task, delay);
+    public static BukkitTask runAsyncLater(Runnable task, long delay) {
+        return Bukkit.getScheduler().runTaskLaterAsynchronously(Utils.get(), task, delay);
     }
 
-    public static void runTimer(Runnable task, long delay, long interval) {
-        Bukkit.getScheduler().runTaskTimer(Utils.get(), task, delay, interval);
+    public static BukkitTask runTimer(Runnable task, long delay, long interval) {
+        return Bukkit.getScheduler().runTaskTimer(Utils.get(), task, delay, interval);
     }
 
-    public static void runAsyncTimer(Runnable task, long delay, long interval) {
-        Bukkit.getScheduler().runTaskTimerAsynchronously(Utils.get(), task, delay, interval);
+    public static BukkitTask runAsyncTimer(Runnable task, long delay, long interval) {
+        return Bukkit.getScheduler().runTaskTimerAsynchronously(Utils.get(), task, delay, interval);
     }
 
 }
