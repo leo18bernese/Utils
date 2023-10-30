6502952a74b1e6a0d494752c4b78fd4f4125a4e4
leo18bernese <leo18bermese@gmail.com>
2023-10-30 22:23:39 +0100

    added get inv contents + armor method

--- bukkit/src/main/java/me/leoo/utils/bukkit/bukkit/BukkitUtils.java
+++ bukkit/src/main/java/me/leoo/utils/bukkit/bukkit/BukkitUtils.java
@@ -1,6 +1,12 @@
 package me.leoo.utils.bukkit.bukkit;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class BukkitUtils {
     public static final String VERSION_STRING = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
@@ -8,5 +14,12 @@ public class BukkitUtils {
 
     public static boolean supports(int version) {
         return VERSION >= version;
+    }
+
+    public static List<ItemStack> getInventoryAndArmor(Player player){
+        List<ItemStack> items = new ArrayList<>(Arrays.asList(player.getInventory().getContents()));
+        items.addAll(Arrays.asList(player.getInventory().getArmorContents()));
+
+        return items;
     }
 }
