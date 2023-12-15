f31217b9941e903e9fa5aa61037093345cb5a155
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2023-12-15 23:38:32 +0100

    added CC#strip

--- bukkit/src/main/java/me/leoo/utils/bukkit/chat/CC.java
+++ bukkit/src/main/java/me/leoo/utils/bukkit/chat/CC.java
@@ -1,5 +1,6 @@
 package me.leoo.utils.bukkit.chat;
 
+import lombok.experimental.UtilityClass;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
@@ -10,40 +11,45 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 
+@UtilityClass
 public class CC {
 
-    public static final String BLUE = "§9";
-    public static final String AQUA = "§b";
-    public static final String YELLOW = "§e";
-    public static final String RED = "§c";
-    public static final String GRAY = "§7";
-    public static final String GOLD = "§6";
-    public static final String GREEN = "§a";
-    public static final String WHITE = "§f";
-    public static final String BLACK = "§0";
-    public static final String BOLD = "§l";
-    public static final String ITALIC = "§o";
-    public static final String UNDER_LINE = "§n";
-    public static final String STRIKE_THROUGH = "§m";
-    public static final String MAGENTA_RGB = "#%%__USER__%%0";
-    public static final String RESET = "§r";
-    public static final String DARK_BLUE = "§1";
-    public static final String DARK_AQUA = "§3";
-    public static final String DARK_GRAY = "§8";
-    public static final String DARK_GREEN = "§2";
-    public static final String DARK_PURPLE = "§5";
-    public static final String DARK_RED = "§4";
-    public static final String PINK = "§d";
+    public final String BLUE = "§9";
+    public final String AQUA = "§b";
+    public final String YELLOW = "§e";
+    public final String RED = "§c";
+    public final String GRAY = "§7";
+    public final String GOLD = "§6";
+    public final String GREEN = "§a";
+    public final String WHITE = "§f";
+    public final String BLACK = "§0";
+    public final String BOLD = "§l";
+    public final String ITALIC = "§o";
+    public final String UNDER_LINE = "§n";
+    public final String STRIKE_THROUGH = "§m";
+    public final String MAGENTA_RGB = "#%%__USER__%%0";
+    public final String RESET = "§r";
+    public final String DARK_BLUE = "§1";
+    public final String DARK_AQUA = "§3";
+    public final String DARK_GRAY = "§8";
+    public final String DARK_GREEN = "§2";
+    public final String DARK_PURPLE = "§5";
+    public final String DARK_RED = "§4";
+    public final String PINK = "§d";
 
-    public static String color(String s) {
+    public String color(String string) {
         if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
-            s = PlaceholderAPI.setPlaceholders(null, s);
+            string = PlaceholderAPI.setPlaceholders(null, string);
         }
 
-        return ChatColor.translateAlternateColorCodes('&', s);
+        return ChatColor.translateAlternateColorCodes('&', string);
     }
 
-    public static TextComponent getClickableMessage(String text, String hover, String command, ClickEvent.Action action) {
+    public String strip(String string) {
+        return ChatColor.stripColor(string);
+    }
+
+    public TextComponent getClickableMessage(String text, String hover, String command, ClickEvent.Action action) {
         TextComponent component = new TextComponent(text);
         component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hover).create()));
         component.setClickEvent(new ClickEvent(action, command));
@@ -51,7 +57,7 @@ public class CC {
         return component;
     }
 
-    public static String translatePluginInfo(Plugin plugin, String s) {
+    public String translatePluginInfo(Plugin plugin, String s) {
         PluginDescriptionFile info = plugin.getDescription();
 
         return s.replace("{name}", info.getName())
