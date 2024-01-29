739014306c3b0b2168827d65961ec9b3c7cde706
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2024-01-29 18:28:46 +0100

    minor optimization adding also exception in some methods

--- common/src/main/java/me/leoo/utils/common/number/NumberUtil.java
+++ common/src/main/java/me/leoo/utils/common/number/NumberUtil.java
@@ -26,77 +26,51 @@ public class NumberUtil {
     }
 
     public int toInt(Object object) {
-        if (object instanceof Number) {
-            return ((Number) object).intValue();
-        }
-
         try {
             return Integer.parseInt(object.toString());
         } catch (NumberFormatException | NullPointerException ignored) {
+            throw new IllegalArgumentException("Cannot convert " + object + " to int");
         }
-
-        return 0;
     }
 
     public float toFloat(Object object) {
-        if (object instanceof Number) {
-            return ((Number) object).floatValue();
-        }
-
         try {
             return Float.parseFloat(object.toString());
         } catch (NumberFormatException | NullPointerException ignored) {
+            throw new IllegalArgumentException("Cannot convert " + object + " to float");
         }
-
-        return 0;
     }
 
     public double toDouble(Object object) {
-        if (object instanceof Number) {
-            return ((Number) object).doubleValue();
-        }
-
         try {
             return Double.parseDouble(object.toString());
         } catch (NumberFormatException | NullPointerException ignored) {
+            throw new IllegalArgumentException("Cannot convert " + object + " to double");
         }
-        return 0;
     }
 
     public long toLong(Object object) {
-        if (object instanceof Number) {
-            return ((Number) object).longValue();
-        }
-
         try {
             return Long.parseLong(object.toString());
         } catch (NumberFormatException | NullPointerException ignored) {
+            throw new IllegalArgumentException("Cannot convert " + object + " to long");
         }
-        return 0;
     }
 
     public short toShort(Object object) {
-        if (object instanceof Number) {
-            return ((Number) object).shortValue();
-        }
-
         try {
             return Short.parseShort(object.toString());
         } catch (NumberFormatException | NullPointerException ignored) {
+            throw new IllegalArgumentException("Cannot convert " + object + " to short");
         }
-        return 0;
     }
 
     public byte toByte(Object object) {
-        if (object instanceof Number) {
-            return ((Number) object).byteValue();
-        }
-
         try {
             return Byte.parseByte(object.toString());
         } catch (NumberFormatException | NullPointerException ignored) {
+            throw new IllegalArgumentException("Cannot convert " + object + " to byte");
         }
-        return 0;
     }
 
     public String toAlphabet(int number) {
