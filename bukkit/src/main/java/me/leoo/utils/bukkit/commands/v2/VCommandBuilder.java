11865df688712acc679bd1616427edcca7e4827e
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2024-02-04 12:01:30 +0100

    custom and dynamic usage (beta)

--- bukkit/src/main/java/me/leoo/utils/bukkit/commands/v2/VCommandBuilder.java
+++ bukkit/src/main/java/me/leoo/utils/bukkit/commands/v2/VCommandBuilder.java
@@ -6,7 +6,9 @@ import me.leoo.utils.bukkit.chat.CC;
 import me.leoo.utils.bukkit.commands.CommandManager;
 import me.leoo.utils.bukkit.commands.v2.annotation.*;
 import me.leoo.utils.bukkit.commands.v2.cache.VCommandCache;
+import me.leoo.utils.bukkit.commands.v2.parser.ArgumentParser;
 import me.leoo.utils.bukkit.commands.v2.tabcomplete.VTabComplete;
+import me.leoo.utils.bukkit.config.ConfigManager;
 import me.leoo.utils.common.reflection.ReflectionUtil;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -32,6 +34,8 @@ public class VCommandBuilder {
 
     private final List<VCommandBuilder> subCommands = new ArrayList<>();
 
+    private static final VCommandManager manager = VCommandManager.get();
+
     public VCommandBuilder(Class<?> clazz) {
         for (Method method : clazz.getMethods()) {
             parseCommand(method);
@@ -50,7 +54,7 @@ public class VCommandBuilder {
             this.executorType = command.executor();
             this.permission = parsePermission(method);
 
-            this.usage = parseUsage(method);
+            this.usage = parseUsage(this.name, method);
 
             this.method = method;
 
@@ -68,7 +72,7 @@ public class VCommandBuilder {
             CommandExecutor subExecutor = subCommand.executor();
             String subPermission = parsePermission(method);
 
-            String subUsage = parseUsage(method);
+            String subUsage = parseUsage(this.name + " " + subName, method);
 
             subCommands.add(new VCommandBuilder(subName, subAliases, subExecutor, subPermission, subUsage, method));
         } else if (method.getAnnotation(TabComplete.class) != null) {
@@ -77,7 +81,7 @@ public class VCommandBuilder {
             String main = tabComplete.value();
             String[] alias = tabComplete.aliases();
 
-            VCommandCache.getTabComplete().put(main, new VTabComplete(name, alias, method));
+            VCommandCache.getTabComplete().put(main, new VTabComplete(this.name, main, alias, method));
         }
     }
 
@@ -91,30 +95,46 @@ public class VCommandBuilder {
         return "";
     }
 
-    private String parseUsage(Method method) {
+    //usage
+    private String parseUsage(String name, Method method) {
         if (method.getAnnotation(CommandUsage.class) != null) {
             CommandUsage commandUsage = method.getAnnotation(CommandUsage.class);
 
             return commandUsage.value();
         }
 
+        return parseUsageFromSettings(name, method);
+    }
+
+    private String parseUsageFromSettings(String name, Method method) {
+        ConfigManager config = manager.getConfigManager();
+        String path = manager.getCommandUsagePath();
+
+        if (config != null && path != null) {
+            return config.getString(path + "." + name);
+        }
+
+        if (manager.isBuildUsageMessage()) {
+            return "&cUsage: /" + name + " " + ArgumentParser.parseArgumentsString(method);
+        }
+
         return null;
     }
 
     //getter
-    public VCommandBuilder getParent(String name) {
+    /*public VCommandBuilder getParent(String name) {
         //make it recursive
         for (VCommandBuilder subCommand : subCommands) {
             System.out.println("checking " + subCommand.getName() + " for " + name);
 
-            if (subCommand.getName().equals(name)/* || getFromAlias(name) != null*/) return subCommand;
+            if (subCommand.getName().equals(name)/* || getFromAlias(name) != null*//*) return subCommand;
 
             VCommandBuilder parent = subCommand.getParent(name);
             if (parent != null) return parent;
         }
 
         return null;
-    }
+    }*/
 
     public VCommandBuilder getFromAlias(String alias) {
         return subCommands.stream().filter(subCommand -> Arrays.asList(subCommand.getAliases()).contains(alias)).findFirst().orElse(null);
@@ -144,19 +164,19 @@ public class VCommandBuilder {
             case PLAYER:
             case OPERATORS:
                 if (!(sender instanceof Player)) {
-                    sender.sendMessage(CC.color(VCommandManager.getError().getOnlyPlayersMessage()));
+                    sender.sendMessage(CC.color(manager.getError().getOnlyPlayersMessage()));
                     return false;
                 }
 
                 if (!sender.isOp() && executorType == CommandExecutor.OPERATORS) {
-                    sender.sendMessage(CC.color(VCommandManager.getError().getOnlyOperatorsMessage()));
+                    sender.sendMessage(CC.color(manager.getError().getOnlyOperatorsMessage()));
                     return false;
                 }
 
                 return true;
             case CONSOLE:
                 if (sender instanceof Player) {
-                    sender.sendMessage(CC.color(VCommandManager.getError().getOnlyConsoleMessage()));
+                    sender.sendMessage(CC.color(manager.getError().getOnlyConsoleMessage()));
                     return false;
                 }
 
@@ -173,8 +193,8 @@ public class VCommandBuilder {
 
     //send usage
     public void sendUsage(CommandSender sender) {
-        if (usage == null) {
-            sender.sendMessage(CC.color(VCommandManager.getError().getMissingArgumentsMessage()));
+        if (usage == null || usage.isEmpty()) {
+            sender.sendMessage(CC.color(manager.getError().getMissingArgumentsMessage()));
             return;
         }
 
