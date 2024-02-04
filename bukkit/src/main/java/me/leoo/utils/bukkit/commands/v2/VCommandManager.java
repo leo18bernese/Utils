66e2dbc9098cfeac10193aace6ade65757b65381
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2024-02-04 11:59:40 +0100

    rewrite

--- bukkit/src/main/java/me/leoo/utils/bukkit/commands/v2/VCommandManager.java
+++ bukkit/src/main/java/me/leoo/utils/bukkit/commands/v2/VCommandManager.java
@@ -1,31 +1,51 @@
 package me.leoo.utils.bukkit.commands.v2;
 
+import lombok.Data;
 import lombok.Getter;
 import me.leoo.utils.bukkit.commands.v2.exception.CommandError;
 import me.leoo.utils.bukkit.commands.v2.exception.VCommandError;
+import me.leoo.utils.bukkit.config.ConfigManager;
 
+@Data
 public class VCommandManager {
 
-    @Getter
-    private static VCommandError error;
+    private final VCommandError error;
+
+    private String commandUsagePath;
+    private ConfigManager configManager;
+
+    private boolean buildUsageMessage = true;
+
+    private static VCommandManager instance;
 
     public VCommandManager(VCommandError commandError) {
+        instance = this;
+
         error = commandError;
     }
 
     public VCommandManager() {
-        error = new CommandError();
+        this(new CommandError());
     }
 
-    public static void register(VCommandError vCommandError) {
-        if (vCommandError == null) {
-            error = new CommandError();
-        } else {
-            error = vCommandError;
+    public void setCommandUsage(ConfigManager configManager, String path) {
+        this.configManager = configManager;
+        this.commandUsagePath = path;
+    }
+
+    public static VCommandManager get() {
+        return instance;
+    }
+
+    /*public static void register(VCommandManager manager, VCommandError vCommandError) {
+        if (instance == null) {
+            instance = manager;
         }
+
+        error = vCommandError == null ? new CommandError() : vCommandError;
     }
 
-    public static void register() {
-        register(null);
-    }
+    public static void register(VCommandManager manager) {
+        register(manager, null);
+    }*/
 }
