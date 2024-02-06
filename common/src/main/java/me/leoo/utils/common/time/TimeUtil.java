3cb9b1ddcba296ab5764e5e6e6f5a55899b24425
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2024-02-06 22:19:10 +0100

    fixed wrong return class type

--- common/src/main/java/me/leoo/utils/common/time/TimeUtil.java
+++ common/src/main/java/me/leoo/utils/common/time/TimeUtil.java
@@ -9,7 +9,7 @@ import java.util.Map;
 @UtilityClass
 public class TimeUtil {
 
-    public int millisFromTimeString(String time) {
+    public long millisFromTimeString(String time) {
         Map<String, Integer> values = new HashMap<>();
 
         for (String s : new String[]{"s", "second", "seconds"}) {
