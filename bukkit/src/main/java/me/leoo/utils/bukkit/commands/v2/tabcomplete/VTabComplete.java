b232860d642dccbd7ce82a4a2bd339959e15f452
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2024-02-09 22:00:28 +0100

    prevent NullPointerException if tab complete isn't correctly setup

--- bukkit/src/main/java/me/leoo/utils/bukkit/commands/v2/tabcomplete/VTabComplete.java
+++ bukkit/src/main/java/me/leoo/utils/bukkit/commands/v2/tabcomplete/VTabComplete.java
@@ -2,9 +2,9 @@ package me.leoo.utils.bukkit.commands.v2.tabcomplete;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import me.leoo.utils.bukkit.commands.CommandManager;
 import me.leoo.utils.bukkit.commands.v2.cache.VCommandCache;
 import me.leoo.utils.common.reflection.ReflectionUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 
 import java.lang.reflect.Method;
@@ -22,7 +22,13 @@ public class VTabComplete {
     private Method method;
 
     public List<String> execute(CommandSender sender, String alias, String[] args) {
-        return (List<String>) ReflectionUtil.invokeMethod(method, VCommandCache.getVInstances().get(mainCommand), sender, alias, args);
+        Object instance = VCommandCache.getVInstances().get(mainCommand);
+        if (instance == null) {
+            Bukkit.getLogger().warning("No instance found for tab complete for main command " + mainCommand);
+            return null;
+        }
+
+        return (List<String>) ReflectionUtil.invokeMethod(method, instance, sender, alias, args);
     }
 
 }
