1c5ee7af03e6a9f13e923529fda198a1393cfc62
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2024-06-21 13:28:40 +0200

    added replacements

--- bukkit/src/main/java/me/leoo/utils/bukkit/bukkit/PlaceholderMap.java
+++ bukkit/src/main/java/me/leoo/utils/bukkit/bukkit/PlaceholderMap.java
@@ -1,6 +1,5 @@
-package me.leoo.utils.bukkit.items;
+package me.leoo.utils.bukkit.bukkit;
 
-import me.leoo.utils.common.number.NumberUtil;
 import me.leoo.utils.common.string.StringUtil;
 
 import java.util.HashMap;
@@ -9,6 +8,8 @@ import java.util.Map;
 import java.util.function.Supplier;
 
 public class PlaceholderMap implements Cloneable {
+
+    private final Map<String, Supplier<String>> replacements = new HashMap<>();
 
     private final Map<String, Supplier<String>> placeholders = new HashMap<>();
     private final Map<String, Supplier<List<String>>> multiLinePlaceholders = new HashMap<>();
@@ -51,8 +52,22 @@ public class PlaceholderMap implements Cloneable {
         return this;
     }
 
+    // Replacements
+    public PlaceholderMap addReplacement(String key, Supplier<String> value) {
+        if (!key.startsWith("{") && !key.endsWith("}")) {
+            key = "{" + key + "}";
+        }
+
+        replacements.put(key, value);
+        return this;
+    }
+
     // Parse
     public String parse(String text) {
+        for (Map.Entry<String, Supplier<String>> entry : replacements.entrySet()) {
+            text = replace(text, entry.getKey(), entry.getValue());
+        }
+
         for (Map.Entry<String, Supplier<String>> entry : placeholders.entrySet()) {
             text = text.replace(entry.getKey(), entry.getValue().get());
         }
@@ -61,7 +76,8 @@ public class PlaceholderMap implements Cloneable {
     }
 
     public List<String> parse(List<String> text) {
-        placeholders.forEach((key, value) -> text.replaceAll(line -> line.replace(key, value.get())));
+        replacements.forEach((key, value) -> text.replaceAll(line -> replace(line, key, value)));
+        placeholders.forEach((key, value) -> text.replaceAll(line -> replace(line, key, value)));
 
         for (Map.Entry<String, Supplier<List<String>>> entry : multiLinePlaceholders.entrySet()) {
             StringUtil.replaceWithList(text, entry.getKey(), entry.getValue().get());
@@ -80,4 +96,12 @@ public class PlaceholderMap implements Cloneable {
         return map;
     }
 
+    // Replace method
+    private String replace(String text, String key, Supplier<String> value) {
+        if (value.get() != null) {
+            return text.replace(key, value.get());
+        }
+
+        return text;
+    }
 }
