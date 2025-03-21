4654f58d6ebda2fbdb7b1f878dba513538508753
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2025-03-21 22:32:02 +0100

    fixed sync/async events, modified Events#call and created Events#callSafe which should synchronously call the event and throw any exception

--- bukkit/src/main/java/me/leoo/utils/bukkit/events/Events.java
+++ bukkit/src/main/java/me/leoo/utils/bukkit/events/Events.java
@@ -2,12 +2,14 @@ package me.leoo.utils.bukkit.events;
 
 import lombok.RequiredArgsConstructor;
 import me.leoo.utils.bukkit.Utils;
+import me.leoo.utils.bukkit.task.Tasks;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.EventExecutor;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
 
@@ -34,7 +36,28 @@ public class Events<T extends Event> implements EventExecutor, Listener {
     }
 
     public static boolean call(Event event) {
-        Bukkit.getPluginManager().callEvent(event);
+        if (Bukkit.getServer().isPrimaryThread()) {
+            Bukkit.getPluginManager().callEvent(event);
+        } else {
+            Tasks.run(() -> Bukkit.getPluginManager().callEvent(event));
+        }
+
+        return event instanceof Cancellable && ((Cancellable) event).isCancelled();
+    }
+
+    public static boolean callSafe(Event event) {
+        if (Bukkit.getServer().isPrimaryThread()) {
+            Bukkit.getPluginManager().callEvent(event);
+        } else {
+            try {
+                Bukkit.getScheduler().callSyncMethod(Utils.get(), () -> {
+                    Bukkit.getPluginManager().callEvent(event);
+                    return null;
+                }).get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         return event instanceof Cancellable && ((Cancellable) event).isCancelled();
     }
 
@@ -45,7 +68,7 @@ public class Events<T extends Event> implements EventExecutor, Listener {
     }
 
     @Override
-    public void execute(Listener listener, Event event) {
+    public void execute(@NotNull Listener listener, @NotNull Event event) {
         handler.accept((T) event);
     }
 
