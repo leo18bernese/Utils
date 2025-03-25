2fde0fad623e4bc37896b29636c2282e8061546f
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2025-03-25 18:51:25 +0100

    added CC#sendMessage with List<CommandSource> senders as input and added CC#sendMessageUUID

--- velocity/src/main/java/me/leoo/utils/velocity/chat/CC.java
+++ velocity/src/main/java/me/leoo/utils/velocity/chat/CC.java
@@ -2,7 +2,13 @@ package me.leoo.utils.velocity.chat;
 
 import com.velocitypowered.api.command.CommandSource;
 import lombok.experimental.UtilityClass;
+import me.leoo.utils.velocity.utils.VelocityUtils;
 import net.kyori.adventure.text.Component;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @UtilityClass
 public class CC {
@@ -13,6 +19,17 @@ public class CC {
 
     public void sendMessage(CommandSource sender, String message) {
         sender.sendMessage(getComponent(message));
+    }
+
+    public void sendMessage(List<CommandSource> senders, String message) {
+        senders.forEach(sender -> sender.sendMessage(getComponent(message)));
+    }
+
+    public void sendMessageUUID(List<UUID> senders, String message) {
+        sendMessage(senders.stream()
+                .map(VelocityUtils::getPlayerByUuid)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList()), message);
     }
 
     public Component getComponent(String text) {
