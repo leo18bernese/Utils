45b53c607960daa7705183c04b3a6abc45af625a
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2025-04-23 18:20:46 +0200

    added VelocityUtils#isConnected and VelocityUtils#tryConnect

--- velocity/src/main/java/me/leoo/utils/velocity/utils/VelocityUtils.java
+++ velocity/src/main/java/me/leoo/utils/velocity/utils/VelocityUtils.java
@@ -1,5 +1,6 @@
 package me.leoo.utils.velocity.utils;
 
+import com.velocitypowered.api.proxy.ConnectionRequestBuilder;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
@@ -7,6 +8,8 @@ import lombok.experimental.UtilityClass;
 import me.leoo.utils.velocity.Utils;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 
 @UtilityClass
 public class VelocityUtils {
@@ -29,5 +32,26 @@ public class VelocityUtils {
 
     public RegisteredServer getServer(String serverName) {
         return Utils.getInstance().getProxy().getServer(serverName).orElse(null);
+    }
+
+    // Player
+    public boolean isConnected(Player player, String serverName) {
+        return player.getCurrentServer().isPresent() &&
+                player.getCurrentServer().get().getServerInfo().getName().equals(serverName);
+    }
+
+    public boolean isConnected(Player player, RegisteredServer server) {
+        return player.getCurrentServer().isPresent() &&
+                player.getCurrentServer().get().getServerInfo().equals(server.getServerInfo());
+    }
+
+    public void tryConnect(Player player, RegisteredServer server, Consumer<ConnectionRequestBuilder.Result> consumer, Consumer<Throwable> throwableConsumer) {
+        CompletableFuture<ConnectionRequestBuilder.Result> future = player.createConnectionRequest(server).connect();
+
+        future.thenAccept(consumer).exceptionally(throwable -> {
+            throwable.printStackTrace();
+            throwableConsumer.accept(throwable);
+            return null;
+        });
     }
 }
