a25d522dcf1c543068241b98a3a6d956073c64e1
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2025-05-24 11:21:36 +0200

    add ifEnabled method to HookManager for conditional plugin execution

--- bukkit/src/main/java/me/leoo/utils/bukkit/hook/HookManager.java
+++ bukkit/src/main/java/me/leoo/utils/bukkit/hook/HookManager.java
@@ -9,12 +9,28 @@ public class HookManager {
 
     /**
      * Check if a plugin is enabled.
+     * Non required plugins only!
      *
      * @param pluginName The name of the plugin to check.
      * @return true if the plugin is enabled, false otherwise.
      */
     public boolean isEnabled(String pluginName) {
         return Bukkit.getServer().getPluginManager().isPluginEnabled(pluginName);
+    }
+
+    /**
+     * Check if a plugin is enabled.
+     * Non required plugins only!
+     *
+     * @param pluginName The name of the plugin to check.
+     * @param runnable The runnable to execute if the plugin is enabled.
+     */
+    public void ifEnabled(String pluginName, Runnable runnable) {
+        if (Bukkit.getServer().getPluginManager().isPluginEnabled(pluginName)) {
+            runnable.run();
+
+            Bukkit.getLogger().info("Hoking into " + pluginName + "...");
+        }
     }
 
     /**
